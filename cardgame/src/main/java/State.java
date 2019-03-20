68ba59ee2217d56188855488050eabbe0d86286d
Peter Mao <pm498@cornell.edu>
2019-03-19 21:29:52 -0400

    States and Traits

--- cardgame/src/main/java/State.java
+++ cardgame/src/main/java/State.java
@@ -3,7 +3,42 @@ enum Orientation {
    FORWARD, RIGHT, LEFT, DOWN;
 }
 
+/**
+ * Stores the physical position of a particular card.
+ */
 public class State {
    private boolean face; //face true is faceup, face false is facedown.
    private Orientation orient;
+
+   /**
+    * Public constructor for State.
+    * @param face Boolean which is true if the face is faceup, false otherwise.
+    * @param orient Orientation of FORWARD, RIGHT, LEFT, DOWN.
+    */
+   public State(boolean face, Orientation orient) {
+      this.orient = orient;
+      this.face = face;
+   }
+
+   /**
+    * @return One of the following: FORWARD, RIGHT, LEFT, DOWN.
+    */
+   public Orientation getOrientation() {
+      return orient;
+   }
+
+   /**
+    * @return True if the card is faceup and false if the card is facedown.
+    */
+   public boolean isFaceup() {
+      return face;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (o == this) return true;
+      if (!(o instanceof State)) return false;
+      State s = (State) o;
+      return s.isFaceup() == this.face && s.getOrientation() == this.orient;
+   }
 }
--- cardgame/src/main/java/Trait.java
+++ cardgame/src/main/java/Trait.java
@@ -1,5 +1,34 @@
-import java.util.LinkedList;
-
+/**
+ * Stores a trait such as the suit of a card or the number on its face, but not an effect.
+ * Some traits may be subtraits of other traits and therefore have a superTrait.
+ * This relationship can not be traced down, only up to the root.
+ * A Trait should only ever be constructed once. If more than one Trait is constructed
+ * with the same name, errors may occur.
+ */
 public class Trait {
-   public String name;
+   public String type;
+   public Trait superTrait;
+
+   /**
+    * Constructor for a root Trait.
+    * @param type The name of the Trait.
+    */
+   public Trait(String type) {
+      this.type = type;
+   }
+
+   /**
+    * Constructor for a Trait with a superTrait.
+    * @param type The name of this Trait.
+    * @param superTrait The superTrait that this Trait is to have.
+    */
+   public Trait(String type, Trait superTrait) {
+      this(type);
+      this.superTrait = superTrait;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      return this == o || this.type.equals(o);
+   }
 }
